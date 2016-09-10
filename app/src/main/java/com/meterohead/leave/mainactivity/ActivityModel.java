2e5c9bbecdb052aa221007a8dacac63dab5f10d2
Grzegorz Wierzchanowski <wierzchanowskig@gmail.com>
2016-09-10 09:15:52 +0200

    Project commit

---
+++ app/src/main/java/com/meterohead/leave/mainactivity/ActivityModel.java
@@ -0,0 +1,10 @@
+package com.meterohead.leave.mainactivity;
+
+/**
+ * Created by Lenovo on 2016-09-09.
+ */
+
+public class ActivityModel {
+    public String helloText;
+
+}
---
+++ app/src/main/java/com/meterohead/leave/mainactivity/MainActivity.java
@@ -0,0 +1,78 @@
+package com.meterohead.leave.mainactivity;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.crash.FirebaseCrash;
+import com.meterohead.leave.R;
+import com.meterohead.leave.databinding.ContentMainBinding;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View container = findViewById(R.id.content_main);
+        ContentMainBinding binding = DataBindingUtil.bind(container);
+        ActivityModel model = new ActivityModel();
+        model.helloText = "Hello World of binding!";
+        binding.setActivity(model);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FirebaseCrash.report(new NullPointerException("test"));
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
